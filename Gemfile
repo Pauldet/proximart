ca964630e1794c67804af291422ff2e4f5aaa4ff
Nicolas Martin-Bouyer <nicolas.martinbouyer@gmail.com>
2020-02-25 13:12:03 +0100

    colors changed and user seed not working because of avatar upload

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '6.0.2.1'
 gem 'redis'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'json'
+require 'open-uri'
+require 'faker'
+
+User.destroy_all
+## Create users
+puts 'Creating 5 fake users...'
+user_array = [1,2,3,4,5]
+
+user_array.each do |i|
+  user_email = "#{i}@test.com"
+  username = Faker::GreekPhilosophers.name+"#{i}"
+  bio = Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false)
+  user = User.new(email: user_email, password: '123456', phone_number:'+33695141564', bio: bio)
+  file = URI.open('https://source.unsplash.com/100x100/?avatar')
+  user.avatar.attach(io: file, filename: "avatar#{i}", content_type: 'image/png')
+  user.save!
+end
+
+
+
+
+
+
+# url = 'https://api.github.com/users/ssaunier'
+# user_serialized = open(url).read
+# user = JSON.parse(user_serialized)
+
+# puts "#{user['name']} - #{user['bio']}"
