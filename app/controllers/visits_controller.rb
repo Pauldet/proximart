961b7f3745f0d56f866953b678b1d46cf7505f49
Paul de Thore <p_dethore@hotmail.com>
2020-02-25 14:09:43 +0100

    generating visits controller

---
+++ app/controllers/visits_controller.rb
@@ -0,0 +1,13 @@
+class VisitsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'visits/index'
+  get 'visits/show'
+  get 'visits/create'
+  get 'visits/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
