25f9fad1e1a705bc58bfd00182905a0df66c9c44
Laurent Binder <laurentbinder@gmail.com>
2020-02-25 13:17:54 +0100

    add username in Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_110304) do
+ActiveRecord::Schema.define(version: 2020_02_25_121258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_110304) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone_number"
     t.text "bio"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
