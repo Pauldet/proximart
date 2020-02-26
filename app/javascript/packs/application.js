2693168e1cddafcc8e4660e5268e74088e6dfb5a
Laurent Binder <laurentbinder@gmail.com>
2020-02-26 20:07:35 +0100

    srolling wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,19 @@ initMapbox();
 
 import {show} from '../plugins/init_showsections'
 show();
+
+
+// other scroller
+
+mobiscroll.card('.demo-card li', {
+    theme: 'ios',
+    themeVariant: 'light'
+});
+
+mobiscroll.scrollview('.demo-card', {
+    theme: 'ios',
+    themeVariant: 'light',
+    layout: 'fixed',
+    itemWidth: 134,
+    snap: false
+});
