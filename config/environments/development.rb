9fcc9ffe0ae15427746f28639f9c56af4acc4185
Nicolas Chardeau <ncparis@hotmail.fr>
2020-03-02 17:39:45 +0100

    create link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.hosts << /\A[a-z0-9]+\.ngrok\.io/
+
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
