1338d3940c4dae6c04c3d28f2ffcfb64921eda99
Nicolas Martin-Bouyer <nicolas.martinbouyer@gmail.com>
2020-03-03 10:33:59 +0100

    fixingslider

--- app/javascript/plugins/init_sliderValue.js
+++ app/javascript/plugins/init_sliderValue.js
@@ -1,18 +1,18 @@
-const initSlider  = () => {
-  $('#customRange1').slider({
-  formatter: function(value) {
-    return 'Current value: ' + value;
-  }
-});
-
-}
-
-
-
-// $('customRange1').slider({
+// const initSlider  = () => {
+//   $('#customRange1').slider({
 //   formatter: function(value) {
 //     return 'Current value: ' + value;
 //   }
 // });
 
-export {initSlider};
+// }
+
+
+
+// // $('customRange1').slider({
+// //   formatter: function(value) {
+// //     return 'Current value: ' + value;
+// //   }
+// // });
+
+// export {initSlider};
