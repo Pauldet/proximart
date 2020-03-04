76e5390394ae2fca8ffb1b293ec57edd30b448b1
Laurent Binder <laurentbinder@gmail.com>
2020-03-04 15:20:07 +0100

    open button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ import { initLetters } from "../plugins/init_letters";
 import { initBToX} from "../plugins/init_btox";
 // import { initFlatpickr } from "../plugins/init_flatpickr";
 import { initHearting } from '../plugins/init_hearting';
-import { initOpenButton } from '../plugins/init_openButton';
+// import { initOpenButton } from '../plugins/init_openButton';
 // import { initSlider } from '../plugins/init_sliderValue';
 // import { initDistanceEx } from '../plugins/init_distanceEx';
 // Calls methods
@@ -37,9 +37,9 @@ if (document.querySelector('.heart')) {
 
 // }
 
-if (document.querySelector('')) {
-  initOpenButton();
-}
+// if (document.querySelector('')) {
+//   initOpenButton();
+// }
 
 initBToX();
 show();
--- app/javascript/plugins/init_openButton.js
+++ app/javascript/plugins/init_openButton.js
@@ -1,9 +1,17 @@
-const initOpenButton = () => {
-  document.querySelector('.open-all').addEventListener('click', (event) => {
+// no need with the reload Nico's searching method //
 
-  })
+// const initOpenButton = () => {
+//   const open = document.querySelector('.open-all')
+//   open.addEventListener('click', (event) => {
+//     open.classList.toggle('event');
+//     if (open.classList.toggle('event')) {
 
-}
+
+//     }
+
+//   })
+
+// }
 
 
-export {initOpenButton};
+// export {initOpenButton};
