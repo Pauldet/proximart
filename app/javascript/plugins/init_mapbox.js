aea2d96b12beaa7ccb59948d83ffab40ef382559
Paul de Thore <p_dethore@hotmail.com>
2020-03-06 15:27:13 +0100

    killing circle on map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,6 +16,7 @@ const mapElement = document.getElementById('map');
     });
   };
 
+
   const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
@@ -29,7 +30,7 @@ const mapElement = document.getElementById('map');
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 25 });
   };
 
   const initMapbox = (lon, lat) => {
@@ -37,8 +38,10 @@ const mapElement = document.getElementById('map');
       const map = buildMap(lon, lat);
       map.addControl(
         new mapboxgl.GeolocateControl({
+          showAccuracyCircle: false,
           positionOptions: {
             enableHighAccuracy: true
+
           },
           trackUserLocation: true
           })
@@ -63,15 +66,15 @@ const mapElement = document.getElementById('map');
 
   function showPosition(position) {
     initMapbox(position.coords.longitude, position.coords.latitude)
-    console.log(position.coords);
+    // console.log(position.coords);
 
     if (document.getElementsByClassName('citymapper-url')[0])  {
       const latlong = `${position.coords.latitude},${position.coords.longitude}`;
-      console.log(latlong);
+      // console.log(latlong);
       var url = document.getElementsByClassName('citymapper-url')[0].dataset["url"];
       var new_url = url.replace('{USER_LATLONG}',latlong);
-      console.log(url);
-      console.log(new_url);
+      // console.log(url);
+      // console.log(new_url);
 
       document.getElementsByClassName('citymapper-url')[0].href=new_url;
     }
@@ -79,12 +82,12 @@ const mapElement = document.getElementById('map');
     if (document.getElementsByClassName('simple_form search')[0]) {
       var lat = document.getElementById("setlat");
       var long = document.getElementById("setlong");
-      console.log(lat)
-      console.log(long)
+      // console.log(lat)
+      // console.log(long)
       lat.value = position.coords.latitude;
       long.value = position.coords.longitude;
-      console.log(lat.value);
-      console.log(long.value);
+      // console.log(lat.value);
+      // console.log(long.value);
 
       if (window.location.href.length < 40) {
         console.log(window.location.href.length)
