3b52abd6fb0f450e94e16cb5b43b420fe7c12162
Laurent Binder <laurentbinder@gmail.com>
2020-09-01 22:11:06 +0200

    uncomment config.assets.check_precompiled_asset = false if it bugs again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  # debug this error "Sprockets::Rails::Helper::AssetNotPrecompiled"
+  # config.assets.check_precompiled_asset = false
+
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
