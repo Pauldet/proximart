a4cff9e6008af7ee80fba735e9e98f1e5acae031
Nicolas Chardeau <ncparis@hotmail.fr>
2020-09-17 19:17:34 +0200

    a effacer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+       :recoverable, :rememberable, :validatable
   has_one_attached :avatar
   has_many :participations
   has_many :messages
